7a965447d94dc752db93afaaa3fd88cf4cc41660
jhonatan-martinez-coder <desarrolladorwebandnet6@gmail.com>
2025-06-12 14:57:57 -0500

    feat: add routes for request website pages
    The added routes handle request for:
    
    * index
    * pages (create using the website administrator)
    * 404 page
    
    Authored-by: Jhonatan Martinez

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Livewire\CreateCustomNavBarOption;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MainController::class, 'index']);
+
+Route::get('/products/{id}/details', [MainController::class, 'productDetails']);
+
+Route::get('/page/{slug}', [MainController::class, 'getPageBySlug']);
