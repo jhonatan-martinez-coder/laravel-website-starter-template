9c136fabf08613cfb9aa6136f58d79c7dd416a8f
jhonatan-martinez-coder <desarrolladorwebandnet6@gmail.com>
2025-06-12 11:26:29 -0500

    feat: add footer section for contact information
    This section has information that goes in the website page footer
    This is mainly contact information
    
    Authored-by: Jhonatan Martinez

---
+++ resources/views/partials/footer-info.blade.php
@@ -0,0 +1,46 @@
+<!-- Footer Start -->
+<div class="container-fluid footer py-5">
+    <div class="container py-5">
+        <div class="row g-5">
+            <div class="col-md-7 col-lg-7 col-xl-4">
+                <div class="footer-item d-flex flex-column">
+                    <h4 class="mb-4 text-white">Comunicate con Nosotros</h4>
+                    <p href=""><i class="fas fa-home me-2"></i> Bocagrande Av. San Martin Cra 2 # 8 – 67</p>
+                    <p href=""><i class="fas fa-envelope me-2"></i> cartagena.trip.tours@hotmail.com</p>
+                    <p href=""><i class="fas fa-phone me-2"></i> +57 314 8510695</p>
+                    <p href="" class="mb-3"><i class="fas fa-phone me-2"></i> +57 317 7417374</p>
+                </div>
+            </div>
+            <!--
+            <div class="col-md-5 col-lg-5 col-xl-4">
+                <div class="footer-item d-flex flex-column">
+                    <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i class="fab fa-twitter"></i></a>
+                    <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i class="fab fa-instagram"></i></a>
+                    <a class="btn-square btn btn-primary rounded-circle mx-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            -->
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
+
+<!-- Copyright Start -->
+<div class="container-fluid copyright text-body py-4">
+    <div class="container">
+        <div class="row g-4 align-items-center">
+            <div class="col-md-6 text-center text-md-end mb-md-0">
+                <i class="fas fa-copyright me-2"></i><a class="text-white" href="#">Trip Tours Cartagena</a>, All right
+                reserved.
+            </div>
+            <div class="col-md-6 text-center text-md-start">Developed By <a class="text-white"
+                    href="https://new.webandnet.us/" target="_blank">Web And Net</a></div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- Copyright End -->
+
+<!-- Back to Top -->
+<a href="#" class="btn btn-primary btn-primary-outline-0 btn-md-square back-to-top"><i class="fa fa-arrow-up"></i></a>
