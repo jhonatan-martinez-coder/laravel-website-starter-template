68bc79180455d216cc2bf718be89634b09643374
jhonatan-martinez-coder <desarrolladorwebandnet6@gmail.com>
2025-06-25 16:59:22 -0500

    refactor: set copyright using environment variable for website title

--- resources/views/partials/footer-info.blade.php
+++ resources/views/partials/footer-info.blade.php
@@ -32,7 +32,7 @@
     <div class="container">
         <div class="row g-4 align-items-center">
             <div class="col-md-6 text-center text-md-end mb-md-0">
-                <i class="fas fa-copyright me-2"></i><a class="text-white" href="#">Trip Tours Cartagena</a>, All right
+                <i class="fas fa-copyright me-2"></i><a class="text-white" href="#">{{env('WEBSITE_TITLE')}}</a>, All right
                 reserved.
             </div>
             <div class="col-md-6 text-center text-md-start">Developed By <a class="text-white"
