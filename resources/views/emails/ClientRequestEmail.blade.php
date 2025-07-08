a519871cb7670bfbc38a7b5eea9be0739ccf56cb
jhonatan-martinez-coder <desarrolladorwebandnet6@gmail.com>
2025-07-08 15:30:44 -0500

    feat: create email templates
    two new templates are included
    
    one themplate is for sending mails from the website contact form to the
    businnes administration area
    
    the other template is for sending the client a mail to nofity him that his
    mail has been received successfully
    
    Authored-by: Jhonatan Martinez

---
+++ resources/views/emails/ClientRequestEmail.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head></head>
+
+<body>
+    <h3>Formulario de Contacto</h3>
+    <p style="margin-bottom: 3rem">Este correo ha sido enviado automaticamente desde el formulario de contacto que se
+        encuentra en el sitio web {{env('WEBSITE_TITLE')}}</p>
+
+    <div style="margin-bottom: 2rem">
+        <p style="margin-bottom: 1rem">A continuación, se listan los datos del usuario:</p>
+        <p><strong>De:</strong> {{ $websiteContactForm['client_email'] }}</p>
+        <p><strong>Nombre:</strong> {{ ucfirst($websiteContactForm['client_name']) }}</p>
+        <p><strong>Asunto:</strong> {{ $websiteContactForm['subject'] }}</p>
+        <p><strong>Mensaje:</strong> {{ $websiteContactForm['message'] }}</p>
+    </div>
+
+   <div style="margin-bottom: 2rem">
+        <address style="margin-top: 2rem;">
+           
+        </address>
+        <div style="padding-top: 1rem;">
+            <p>Correo: </p>  
+            <p>Correo: </p>
+        </div>
+    </div>
+
+    @php
+        $imagePath = public_path('main-website-logo.png');
+        $image = file_get_contents($imagePath);
+    @endphp
+
+    <img src={{ $message->embedData($image, 'main-website-logo.png') }} alt="main-website-logo" style="width: 300px;">
+</body>
+
+</html>
