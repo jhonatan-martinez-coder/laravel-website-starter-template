7931de514cb40fd6a5f6540688f6c28f3bbf946e
jhonatan-martinez-coder <desarrolladorwebandnet6@gmail.com>
2025-07-09 10:50:22 -0500

    refactor: update footer structure
    the footer structure was updated in order to include bussiness image
    logo and new contact information. also the social media section has had
    some minor layout changes
    
    Authored-by: Jhonatan Martinez

--- resources/views/partials/footer-info.blade.php
+++ resources/views/partials/footer-info.blade.php
@@ -1,22 +1,27 @@
 <!-- Footer Start -->
-<div class="container-fluid footer py-5">
+<div class="container-fluid footer py-5 bg-light">
     <div class="container py-5">
         <div class="row g-5">
-            <div class="col-md-7 col-lg-7 col-xl-4">
+            <div class="col-md-4 col-lg-4 col-xl-4">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4">Comunicate con Nosotros</h4>
                     <p href=""><i class="fas fa-home me-2"></i>address</p>
                     <p href=""><i class="fas fa-envelope me-2"></i> example@hotmail.com</p>
-                    <p href="" class="mb-3"><i class="fas fa-phone me-2"></i> 555-555-555</p>
                     <p href=""><i class="fas fa-phone me-2"></i> 555-555-555</p>
                 </div>
             </div>
-            <div class="col-md-5 col-lg-5 col-xl-4">
-                <div class="footer-item d-flex flex-column">
+            <div class="col-md-4 col-lg-4 col-xl-4">
+                <h4 class="mb-4">Siguenos</h4>
+                <div class="footer-item d-flex">
                     <a class="btn-square btn btn-primary rounded-circle mx-1 my-1" href=""><i class="fab fa-facebook-f"></i></a>
                     <a class="btn-square btn btn-primary rounded-circle mx-1 my-1" href=""><i class="fab fa-twitter"></i></a>
                     <a class="btn-square btn btn-primary rounded-circle mx-1 my-1" href=""><i class="fab fa-instagram"></i></a>
                     <a class="btn-square btn btn-primary rounded-circle mx-1 my-1" href=""><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+             <div class="col-md-4 col-lg-4 col-xl-4 p-0">
+                <div class="footer-img">
+                    <img src={{ asset('main-website-logo.png') }} alt="main-website-logo">
                 </div>
             </div>
         </div>
